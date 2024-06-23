1b0f4ed89fabd952b70fcaa7cc9241b02b5fe301
kavya19980101 <kavyastestings@gmail.com>
2024-06-23 22:35:33 +0530

    Programming challenge

---
+++ Maxof3.java
@@ -0,0 +1,11 @@
+
+//Program of Maximum of 3 numbers using Ternary Operator
+public class Maxof3 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        int max=a>b?(a>c?a:c):(b>c?b:c);
+        System.out.println("the max of number is "+max);//Terary Operator
+    }
+}
