11d9330e23309d023683eb51d5b5be358252b7d2
kavya19980101 <kavyastestings@gmail.com>
2024-06-28 19:42:46 +0530

    Programming challenge

---
+++ Program13_FibonaciiSeries.java
@@ -0,0 +1,17 @@
+public class Program13_FibonaciiSeries {
+    public static void main(String[] args) {
+        int num1=0;
+        int num2=1;
+        int sum=0;
+        System.out.println(num1);
+        System.out.println(num2);
+        for(int i=2;i<=10;i++)
+        {
+           sum=num1+num2;
+          System.out.println(sum);
+          num1=num2;
+          num2=sum;
+        }
+
+    }
+}
