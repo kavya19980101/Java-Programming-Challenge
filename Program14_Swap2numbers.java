cdde09f894f15983ffba57b31c5bda741d95e068
kavya19980101 <kavyastestings@gmail.com>
2024-06-28 22:53:58 +0530

    Programming challenge

---
+++ Program14_Swap2numbers.java
@@ -0,0 +1,10 @@
+public class Program14_Swap2numbers {
+    public static void main(String[] args) {
+        int a=10,b=20;
+        a=a*b;
+        b=a/b;
+        a=a/b;
+        System.out.println("a value: "+a);
+        System.out.println("b value: "+b);
+    }
+}
