8b343befb278999e032e0b890df06be24723a6ba
kavya19980101 <kavyastestings@gmail.com>
2024-06-28 19:28:52 +0530

    Programming challenge

---
+++ Program12_Pslindromenumber.java
@@ -0,0 +1,25 @@
+public class Program12_Pslindromenumber {
+        public static void main(String[] args) {
+            int num=121;
+            int org_num=num;
+            int rev=0;
+
+            while(num!=0)
+            {
+                rev=rev*10+num%10;
+                num=num/10;
+            }
+            System.out.println(rev);
+            if(org_num == rev ) {
+                System.out.println("palindrome");
+            }
+            else {
+                System.out.println("Not a palindrome");}
+
+
+        }
+
+
+    }
+
+
